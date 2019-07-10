a441f59eff95924b7b7e249fc689d6b829bc3424
Andy Strube <andystrube64@gmail.com>
2019-07-10 12:52:52 -0700

    Another refactor, more methods

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -6,23 +6,27 @@ class RPN
     @stack = Stack.new
   end
 
-  def evaluate
-    return 0 if @string == ""
-    build.each do |num_or_operator|
-      if operator?(num_or_operator)
-        operand1 = @stack.pop.value.to_i
-        operand2 = @stack.pop.value.to_i
-        @stack.push(operand2.send(num_or_operator, operand1))
-      else
-        @stack.push(num_or_operator)
-      end
-    end
-    @stack.pop.value
-  end
+  # def evaluate
+  #   return 0 if @string == ""
+  #   build.each do |num_or_operator|
+  #     calculation(num_or_operator)
+  #   end
+  #   @stack.pop.value
+  # end
 
-  def build
-    @string.split(' ')
-  end
+  # def calculation(num_or_operator)
+  #   if operator?(num_or_operator)
+  #     operand1 = @stack.pop.value.to_i
+  #     operand2 = @stack.pop.value.to_i
+  #     @stack.push(operand2.send(num_or_operator, operand1))
+  #   else
+  #     @stack.push(num_or_operator)
+  #   end
+  # end
+
+  # def build
+  #   @string.split(' ')
+  # end
 
   # def evaluate
   #   return 0 if @string == ""
@@ -42,16 +46,37 @@ class RPN
   #   numbers.pop
   # end
 
-  # def build
-  #   char_array = @string.split(' ').reverse
-  #   char_array.each do |char|
-  #     if operator?(char)
-  #       @stack.push(char)
-  #     else
-  #       @stack.push(char.to_i)
-  #     end
-  #   end
-  # end
+  def evaluate
+    return 0 if @string == ""
+    @build = build
+    current = @stack.data
+    while current
+      calculation(current)
+      current = current.next_node
+    end
+    @build.pop
+  end
+
+  def calculation(node)
+    if operator?(node.value)
+      operand1 = @build.pop
+      operand2 = @build.pop
+      @build.push(operand2.send(node.value, operand1))
+    else
+      @build.push(node.value)
+    end
+  end
+
+  def build
+    char_array = @string.split(' ').reverse
+    char_array.each do |char|
+      if operator?(char)
+        @stack.push(char)
+      else
+        @stack.push(char.to_i)
+      end
+    end
+  end
 
   def operator?(operator_string)
     case operator_string
