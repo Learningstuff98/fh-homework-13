0f3e051b97a70f924db7961cf7b96838bc13f1fd
Andy Strube <andystrube64@gmail.com>
2019-07-12 18:44:11 -0700

    Refactored messages for some of the tests

--- spec/rpn_calculator_spec.rb
+++ spec/rpn_calculator_spec.rb
@@ -26,68 +26,68 @@ RSpec.describe RPN, type: :model do
 
   end
 
-  describe 'operator? should work' do
+  describe 'operator? should' do
 
-    it 'should determine if a plus sign is an operator or not' do
+    it 'determine if a string plus sign is an operator or not' do
       rpn = RPN.new("")
       expect(rpn.operator?('+')).to be true
     end
 
-    it 'should determine if a minus sign is an operator or not' do
+    it 'determine if a string minus sign is an operator or not' do
       rpn = RPN.new("")
       expect(rpn.operator?('-')).to be true
     end
 
-    it 'should determine if an astrix is an operator or not' do
+    it 'determine if a string astrix is an operator or not' do
       rpn = RPN.new("")
       expect(rpn.operator?('*')).to be true
     end
 
-    it 'should determine if a slash is an operator or not' do
+    it 'determine if a string slash is an operator or not' do
       rpn = RPN.new("")
       expect(rpn.operator?('/')).to be true
     end
 
-    it 'should determine if a character is an operator or not' do
+    it 'determine if a string 3 is an operator or not' do
       rpn = RPN.new("")
       expect(rpn.operator?('3')).to be false
     end
 
-    it 'should determine if a character is an operator or not' do
+    it 'determine if a string 200 is an operator or not' do
       rpn = RPN.new("")
       expect(rpn.operator?('200')).to be false
     end
 
-    it 'should determine if a character is an operator or not' do
+    it 'determine if a string 10 is an operator or not' do
       rpn = RPN.new("")
       expect(rpn.operator?('10')).to be false
     end
 
   end
 
-  describe 'evaluate should work' do
+  describe 'evaluate' do
 
     it 'should return 0 for an empty string' do
       rpn = RPN.new("")
       expect(rpn.evaluate).to be 0
     end
 
-    it 'should return 3 for this 1 2 +' do 
+    it 'should return 3 for: 1 2 +' do 
       rpn = RPN.new("1 2 +")
       expect(rpn.evaluate).to be 3
     end
 
-    it 'should return 9 for this string' do
+    it 'should return 9 for: 3 2 1 + *' do
       rpn = RPN.new("3 2 1 + *")
       expect(rpn.evaluate).to be 9
     end
 
-    it 'should return 7 for this string' do
+    it 'should return 7 for: 1 2 3 * +' do
       rpn = RPN.new("1 2 3 * +")
       expect(rpn.evaluate).to be 7
     end
 
-    it 'should return 5 for this string' do
+    it 'should return 5 for: 15 7 1 1 + - / 3 * 2 1 1 + + -' do
       rpn = RPN.new("15 7 1 1 + - / 3 * 2 1 1 + + -")
       expect(rpn.evaluate).to be 5
     end
